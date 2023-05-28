859938ea4eb9e803b12fa46d6a191840d04379d4
Marco Herazo <marcoherazo11@gmail.com>
2023-05-28 19:19:35 -0300

    MR-55: create new modals and add funcionality

--- src/Components/Trainers/Modal/index.js
+++ src/Components/Trainers/Modal/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './modals.module.css';
+import styles from './modal.module.css';
 
 const ConfirmationModal = ({ message, onConfirm, onCancel }) => {
   return (
--- src/Components/Trainers/Table/index.js
+++ src/Components/Trainers/Table/index.js
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './table.module.css';
+import ConfirmationModal from '../Modal';
 
 const Table = ({ data, deleteItem }) => {
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [selectedTrainer, setSelectedTrainer] = useState(null);
+
+  const handleDeleteConfirmation = () => {
+    if (selectedTrainer) {
+      deleteItem(selectedTrainer._id);
+      setShowConfirmationModal(false);
+      setSelectedTrainer(null);
+    }
+  };
+
+  const handleCancelConfirmation = () => {
+    setShowConfirmationModal(false);
+    setSelectedTrainer(null);
+  };
+
+  const openConfirmationModal = (trainer) => {
+    setSelectedTrainer(trainer);
+    setShowConfirmationModal(true);
+  };
+
   return (
-    <table className={styles.tableContainer}>
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => {
-          return (
-            <tr key={item.id}>
-              <td>{item.firstName}</td>
-              <td>{item.lastName}</td>
-              <td></td>
-              <td>
-                <button className={styles.editButton}>Edit</button>
-              </td>
-              <td>
-                <button className={styles.deleteButton} onClick={() => deleteItem(item._id)}>
-                  X
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <table className={styles.tableContainer}>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th></th>
+            <th>Actions</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr key={item._id}>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td></td>
+                <td>
+                  <button className={styles.editButton}>Edit</button>
+                </td>
+                <td>
+                  <button
+                    className={styles.deleteButton}
+                    onClick={() => openConfirmationModal(item)}
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {showConfirmationModal && (
+        <ConfirmationModal
+          message={`Are you sure you want to delete the trainer ${selectedTrainer.firstName} ${selectedTrainer.lastName}?`}
+          onConfirm={handleDeleteConfirmation}
+          onCancel={handleCancelConfirmation}
+        />
+      )}
+    </div>
   );
 };
 
