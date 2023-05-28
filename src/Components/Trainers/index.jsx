1b5ec3fa2e32005e22a790637e765a7308e63ab9
Marco Herazo <marcoherazo11@gmail.com>
2023-05-28 13:49:31 -0300

    MR-55: create list for trainers

--- src/Components/Trainers/index.jsx
+++ src/Components/Trainers/index.jsx
@@ -1,11 +1,27 @@
 import styles from './trainers.module.css';
+import Table from './Table';
+/* import Modal from './Modal'; */
+import { useEffect, useState } from 'react';
 
-function Trainers() {
+const Trainers = () => {
+  const [trainers, setTrainers] = useState([]);
+  const getTrainers = async () => {
+    const response = await fetch('https://nai-megarocket-server.vercel.app/api/trainers');
+    const data = await response.json();
+    setTrainers(data.data);
+  };
+  useEffect(() => {
+    getTrainers();
+  }, []);
+  const deleteItem = (id) => {
+    setTrainers([...trainers.filter((trainer) => trainer._id !== id)]);
+  };
   return (
     <section className={styles.container}>
       <h2>Trainers</h2>
+      <Table data={trainers} deleteItem={deleteItem} />
     </section>
   );
-}
+};
 
 export default Trainers;
