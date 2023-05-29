28620ac0c7aec31473f9638f38b004d9cc5102b6
Federico Peralta <fedeper100@hotmail.com>
2023-05-29 12:17:53 -0300

    MR-63: styles corrected

--- src/Components/Subscriptions/Table/index.jsx
+++ src/Components/Subscriptions/Table/index.jsx
@@ -10,24 +10,27 @@ const Table = ({ data, deleteItem, setShowModal }) => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr className={styles.trHeadContainer}>
-          <th>Days</th>
-          <th>Member</th>
+    <table className={styles.subscriptionsTable}>
+      <thead className={styles.theadContainer}>
+        <tr className={styles.trContainer}>
+          <th>First name</th>
+          <th>Last name</th>
+          <th className={styles.thDaysContainer}>Days</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         {data.map((item) => {
           return (
             <tr key={item._id}>
-              <td>{item.classes.day}</td>
-              <td className={styles.nameContainer}>{item.member.firstName}</td>
+              <td className={styles.tdFirstNameContainer}>{item.member.firstName}</td>
+              <td>{item.member.lastName}</td>
+              <td>{item.classes.day[0] + ' - ' + item.classes.day[1]}</td>
               <td className={styles.buttonsContainer}>
                 <button className={styles.buttonDelete} onClick={() => handleDelete(item._id)}>
                   <FaTimes />
                 </button>
-                <button className={styles.buttonDelete}>
+                <button className={styles.buttonEdit}>
                   <FaEdit />
                 </button>
               </td>
