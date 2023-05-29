a8bc90bbbde1c3d2540fefc3ecd2388d23361f97
Gonzarey96 <gonzareybet@gmail.com>
2023-05-28 21:51:50 -0300

    MR-61: added js logic

--- src/Components/Members/index.js
+++ src/Components/Members/index.js
@@ -1,25 +1,108 @@
 import { useEffect, useState } from 'react';
-import styles from './members.module.css';
+import styles from './Tables/members.module.css';
+import MembersTable from './Tables/MembersTable';
+import SelectedMemberInfo from './Tables/SelectedMemberInfo';
+import { fetchMembers, deleteMember } from './api';
+import Modal from './Modals/Modal';
 
 function Members() {
   const [members, setMembers] = useState([]);
+  const [selectedMember, setSelectedMember] = useState(null);
+  const [showActions, setShowActions] = useState({});
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  const [memberToDelete, setMemberToDelete] = useState(null);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((response) => {
-        setMembers(response);
-      });
+    const getMembers = async () => {
+      try {
+        const members = await fetchMembers();
+        setMembers(members);
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+      }
+    };
+
+    getMembers();
   }, []);
+
+  const handleDelete = async (id) => {
+    setIsConfirmOpen(true);
+    setMemberToDelete(id);
+  };
+
+  const handleConfirmDelete = async () => {
+    setIsConfirmOpen(false);
+    try {
+      await deleteMember(memberToDelete);
+      setMembers((prevMembers) => prevMembers.filter((member) => member._id !== memberToDelete));
+      setSuccess('Member deleted successfully');
+    } catch (error) {
+      console.error(error);
+      setError('Failed to delete member');
+    }
+  };
+
+  const handleRowClick = (member) => {
+    setSelectedMember(member);
+    setShowActions({});
+  };
+
+  const handleShowActionsClick = (event, id) => {
+    event.stopPropagation();
+    setShowActions((prevShowActions) => ({
+      ...prevShowActions,
+      [id]: !prevShowActions[id]
+    }));
+  };
+
+  const handleEdit = (member) => {
+    setSelectedMember(member);
+  };
+
+  const handleClose = () => {
+    setSelectedMember(null);
+  };
 
   return (
     <section className={styles.container}>
       <h2>Members</h2>
-      <div>
-        {members.map((employee) => {
-          return <div key={employee.id}>{employee.name}</div>;
-        })}
-      </div>
+      <MembersTable
+        members={members}
+        handleRowClick={handleRowClick}
+        handleShowActionsClick={handleShowActionsClick}
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
+        showActions={showActions}
+      />
+      <button
+        onClick={() => {
+          setSelectedMember(null);
+        }}
+      >
+        Create Member
+      </button>
+      {selectedMember && (
+        <>
+          <SelectedMemberInfo selectedMember={selectedMember} />
+          <button onClick={handleClose}>Close</button>
+        </>
+      )}
+      <Modal
+        isOpen={isConfirmOpen}
+        closeModal={() => setIsConfirmOpen(false)}
+        onConfirm={handleConfirmDelete}
+      >
+        Do you want to delete this member?
+      </Modal>
+      <Modal isOpen={success !== null} closeModal={() => setSuccess(null)}>
+        {success}
+      </Modal>
+      <Modal isOpen={error !== null} closeModal={() => setError(null)}>
+        {error}
+      </Modal>
     </section>
   );
 }
