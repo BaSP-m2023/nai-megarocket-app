923c85c09ea52b984730e76c9ab1b1bca015b9e9
EmaBaldor <emabaldor@gmail.com>
2023-05-30 18:39:22 -0300

    MR-58: trainer fix name function

--- src/Components/Trainers/index.jsx
+++ src/Components/Trainers/index.jsx
@@ -24,7 +24,7 @@ const Trainers = () => {
   useEffect(() => {
     getTrainers();
   }, []);
-  const deleteModal = () => {
+  const modal = () => {
     const handleCloseModal = () => {
       setShowModal(false);
     };
@@ -131,7 +131,7 @@ const Trainers = () => {
             Add New Trainer
           </button>
           <Table data={trainers} deleteItem={deleteItem} editItem={editItem} />
-          {showModal && deleteModal()}
+          {showModal && modal()}
         </>
       )}
     </section>
