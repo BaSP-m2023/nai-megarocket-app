920d1cac8f09134b0afc928346595f9d8eece611
Marco Herazo <marcoherazo11@gmail.com>
2023-05-30 13:29:39 -0300

    MR-55: resolve conflict

---
+++ src/Components/Members/api.jsx
@@ -0,0 +1,28 @@
+async function fetchMembers() {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/members`);
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function deleteMember(id) {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/members/${id}`, {
+      method: 'DELETE'
+    });
+    if (!response.ok) {
+      throw new Error('An error occurred while trying to delete the member');
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+export default {
+  fetchMembers,
+  deleteMember
+};
