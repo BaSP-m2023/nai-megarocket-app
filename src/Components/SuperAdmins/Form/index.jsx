ef014215f1310d7e513e5124066957949a4c3cad
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2023-05-30 21:14:04 -0300

    MR-49: Added styles

--- src/Components/SuperAdmins/Form/index.jsx
+++ src/Components/SuperAdmins/Form/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import styles from './super-admins.module.css';
 
 const Form = (props) => {
   if (!props.showCreateForm) {
@@ -21,39 +22,53 @@ const Form = (props) => {
   const onSubmitPost = (e) => {
     e.preventDefault();
     props.addSuperAdmin(superAdmin);
+    props.closeCreateForm();
   };
 
   return (
-    <form onSubmit={onSubmitPost}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={superAdmin.firstName}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" value={superAdmin.email} onChange={onChange} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={superAdmin.password}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <input type="submit" />
-      </div>
-      <button onClick={props.closeCreateForm}>Close</button>
-    </form>
+    <div className={styles.modalContainer}>
+      <form className={styles.modalContent} onSubmit={onSubmitPost}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={superAdmin.firstName}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={superAdmin.email}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={superAdmin.password}
+            onChange={onChange}
+          />
+        </div>
+        <div className={styles.modalButtons}>
+          <input className={styles.modalButton} type="submit" value="Create" />
+          <button className={styles.modalButton} onClick={props.closeCreateForm}>
+            Close
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/Components/SuperAdmins/FormEdit/index.jsx
+++ src/Components/SuperAdmins/FormEdit/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import styles from './super-admins.module.css';
 
 const FormEdit = (props) => {
   if (!props.showEditForm) {
@@ -22,39 +23,53 @@ const FormEdit = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     props.editSuperAdmin(props.superAdminId, superAdmin);
+    props.closeEditForm();
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="name">Namsadsae</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={superAdmin.firstName}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" value={superAdmin.email} onChange={onChange} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={superAdmin.password}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <input type="submit" />
-      </div>
-      <button onClick={props.closeEditForm}>Close</button>
-    </form>
+    <div className={styles.modalContainer}>
+      <form className={styles.modalContent} onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={superAdmin.firstName}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={superAdmin.email}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={superAdmin.password}
+            onChange={onChange}
+          />
+        </div>
+        <div className={styles.modalButtons}>
+          <input className={styles.modalButton} type="submit" value="Edit" />
+          <button className={styles.modalButton} onClick={props.closeEditForm}>
+            Close
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
