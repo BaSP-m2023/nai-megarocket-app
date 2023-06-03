9f9946eec867712e6bdd6878d1198be8ebc0fe87
Federico Peralta <fedeper100@hotmail.com>
2023-06-03 19:07:02 -0300

    MR-77: shared-modal created and added styles

---
+++ src/Components/Shared/Modal/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './modal.module.css';
+
+function SharedModal(props) {
+  if (!props.show) {
+    return null;
+  }
+
+  const onCloseModal = () => {
+    props.closeModal();
+  };
+
+  const onConfirm = () => {
+    props.onConfirm();
+  };
+
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modalContent}>
+        <h3>{props.title}</h3>
+        <p>{props.body}</p>
+        <div className={styles.buttonContainer}>
+          {props.isDelete ? (
+            <>
+              <button className={styles.buttonCancel} onClick={onCloseModal}>
+                Cancel
+              </button>
+              <button className={styles.buttonConfirm} onClick={onConfirm}>
+                Confirm
+              </button>
+            </>
+          ) : (
+            <button className={styles.noButton} onClick={onCloseModal}>
+              Ok
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SharedModal;
