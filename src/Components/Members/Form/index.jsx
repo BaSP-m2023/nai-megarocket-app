b66342668e1c49416ff7239a82980b532d7a4ff0
Alejandro Schmidt <alemusho@gmail.com>
2023-06-05 18:17:58 -0300

    MR-75: logic moved to new routes done

---
+++ src/Components/Members/Form/index.jsx
@@ -0,0 +1,312 @@
+import { useState, useEffect } from 'react';
+import styles from './form.module.css';
+import { useHistory, useParams } from 'react-router-dom';
+
+const MemberForm = () => {
+  const { id } = useParams();
+
+  const history = useHistory();
+
+  // const [firstName, setFirstName] = useState('');
+  // const [lastName, setLastName] = useState('');
+  // const [dni, setDni] = useState('');
+  // const [phone, setPhone] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [city, setCity] = useState('');
+  // const [birthDay, setBirthDay] = useState('');
+  // const [postalCode, setPostalCode] = useState('');
+  // const [isActive, setIsActive] = useState(false);
+  // const [membership, setMembership] = useState('');
+
+  useEffect(() => {
+    if (id) {
+      getMembersById(id);
+
+      // setFirstName(member.firstName);
+      // setLastName(member.lastName);
+      // setDni(member.dni);
+      // setPhone(member.phone);
+      // setEmail(member.email);
+      // setPassword(member.password);
+      // setCity(member.city);
+      // setBirthDay(member.birthDay);
+      // setPostalCode(member.postalCode);
+      // setIsActive(member.isActive);
+      // setMembership(member.membership);
+      // } else {
+      //   setFirstName('');
+      //   setLastName('');
+      //   setDni('');
+      //   setPhone('');
+      //   setEmail('');
+      //   setPassword('');
+      //   setCity('');
+      //   setBirthDay('');
+      //   setPostalCode('');
+      //   setIsActive(false);
+      //   setMembership('');
+    }
+  }, [id]);
+
+  // const handleSubmit = (event) => {
+  //   onSubmit({
+  //     ...member,
+  //     firstName,
+  //     lastName,
+  //     dni,
+  //     phone,
+  //     email,
+  //     password,
+  //     city,
+  //     birthDay,
+  //     postalCode,
+  //     isActive,
+  //     membership
+  //   });
+  // };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // onSubmit({
+    //   ...member,
+    //   firstName,
+    //   lastName,
+    //   dni,
+    //   phone,
+    //   email,
+    //   password,
+    //   city,
+    //   birthDay,
+    //   postalCode,
+    //   isActive,
+    //   membership
+    // });
+    if (id) {
+      updateMemberById(id);
+    } else {
+      createMember(member);
+    }
+  };
+
+  const [member, setMember] = useState({
+    firstName: '',
+    lastName: '',
+    dni: '',
+    phone: '',
+    email: '',
+    password: '',
+    city: '',
+    birthDay: '',
+    postalCode: '',
+    // isActive: false,
+    membership: ''
+  });
+
+  const onChange = (e) => {
+    setMember({
+      ...member,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const getMembersById = async (id) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/members/${id}`);
+      const data = await response.json();
+      setMember(data.data);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const updateMemberById = async (id) => {
+    try {
+      const memberWithoutId = { ...member };
+      delete memberWithoutId._id;
+      delete memberWithoutId.__v;
+      delete memberWithoutId.isActive;
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/members/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(memberWithoutId)
+      });
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        console.error(errorMessage);
+        throw new Error(`An error occurred while trying to update the member: ${errorMessage}`);
+      }
+      history.push('/members');
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const createMember = async (member) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/members`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(member)
+      });
+      if (response.ok) {
+        history.push('/members');
+      } else {
+        throw new Error('An error occurred while trying to add the member');
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  // const handleUpdate = async (member) => {
+  //   try {
+  //     await updateMember(member);
+  //     setMembers((prevMembers) =>
+  //       prevMembers.map((prevMember) => (prevMember._id === member._id ? member : prevMember))
+  //     );
+  //     setSuccess('Member updated successfully');
+  //   } catch (error) {
+  //     console.error(error);
+  //     setErrorMessage('Failed to update member');
+  //   }
+  // };
+
+  return (
+    <div className={styles['form-container']}>
+      <form className={styles['form-form']} onSubmit={handleSubmit}>
+        <label className={styles['form-label']}>
+          Name:
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={member.firstName}
+            onChange={onChange}
+            name="firstName"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          Last Name:
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={member.lastName}
+            onChange={onChange}
+            name="lastName"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          DNI:
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={member.dni}
+            onChange={onChange}
+            name="dni"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          Phone:
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={member.phone}
+            onChange={onChange}
+            name="phone"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          Email:
+          <input
+            className={styles['form-input']}
+            type="email"
+            value={member.email}
+            onChange={onChange}
+            name="email"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          Password:
+          <input
+            className={styles['form-input']}
+            type="password"
+            value={member.password}
+            onChange={onChange}
+            name="password"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          City:
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={member.city}
+            onChange={onChange}
+            name="city"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          Date of birth:
+          <input
+            className={styles['form-input']}
+            type="date"
+            value={member.birthDay}
+            onChange={onChange}
+            name="birthDay"
+          />
+        </label>
+        <label className={styles['form-label']}>
+          ZIP code:
+          <input
+            className={styles['form-input']}
+            type="text"
+            value={member.postalCode}
+            onChange={onChange}
+            name="postalCode"
+          />
+        </label>
+        {/* <label className={styles['form-label']}>
+          Is Active?
+          <input
+            className={styles['form-input']}
+            type="checkbox"
+            checked={member.isActive}
+            onChange={onChange}
+            name="isActive"
+          />
+        </label> */}
+        <label className={styles['form-label']}>
+          Memberships:
+          <select
+            className={styles['form-select']}
+            value={member.membership}
+            onChange={onChange}
+            name="membership"
+          >
+            <option value="">Select an option</option>
+            <option value="Black">Black</option>
+            <option value="Gold">Gold</option>
+            <option value="Silver">Silver</option>
+          </select>
+        </label>
+        <div className={styles['button-container']}>
+          <button className={styles['form-button']} type="submit">
+            submit
+          </button>
+
+          <button className={styles['form-button']} type="button" onClick={() => history.goBack()}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default MemberForm;
