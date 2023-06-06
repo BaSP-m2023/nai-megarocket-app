cf6b3b8365d625c46969d2fc550fcdcae494f3dc
Marco Herazo <marcoherazo11@gmail.com>
2023-06-06 19:13:12 -0300

    MR-84: update files

--- src/Components/Subscriptions/index.jsx
+++ src/Components/Subscriptions/index.jsx
@@ -2,49 +2,14 @@ import React, { useEffect, useState } from 'react';
 import styles from './subscriptions.module.css';
 import Table from './Table';
 import Modal from './Modal';
-import Form from './Form';
+import { useHistory } from 'react-router-dom';
 
 const Subscriptions = () => {
+  const history = useHistory();
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
   const [subscriptions, setSubscriptions] = useState([]);
-  const [classes, setClasses] = useState([]);
-  const [members, setMembers] = useState([]);
-  const [selectedSubscriptionId, setSelectedSubscriptionId] = useState(null);
-  const [selectedSubscription, setSelectedSubscription] = useState(null);
-  const [method, setMethod] = useState('');
-  const [showForm, setShowForm] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
-
-  const addSubscription = async ({ member: newMember, classes }) => {
-    const newSubscription = {
-      classes,
-      member: newMember,
-      date: new Date()
-    };
-
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/subscriptions`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newSubscription)
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to add subscription');
-      }
-
-      const { data } = await response.json();
-
-      setSubscriptions([...subscriptions, data]);
-      setSuccessMessage('Subscription added successfully');
-      getSubscriptions();
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const getSubscriptions = async () => {
     try {
@@ -54,32 +19,6 @@ const Subscriptions = () => {
       }
       const { data } = await response.json();
       setSubscriptions(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const getClasses = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/classes`);
-      if (!response.ok) {
-        throw new Error('Failed to get classes');
-      }
-      const { data } = await response.json();
-      setClasses(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const getMembers = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/members`);
-      if (!response.ok) {
-        throw new Error('Failed to get members');
-      }
-      const { data } = await response.json();
-      setMembers(data);
     } catch (error) {
       console.error(error);
     }
@@ -98,68 +37,21 @@ const Subscriptions = () => {
     }
   };
 
-  const updateSubscription = async (updatedSubscription) => {
-    try {
-      if (!selectedSubscription || !selectedSubscription._id) {
-        throw new Error('Invalid subscription ID');
-      }
-
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/api/subscriptions/${selectedSubscription._id}`,
-        {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(updatedSubscription)
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Failed to update subscription');
-      }
-
-      setSuccessMessage('Subscription updated successfully');
-      getSubscriptions();
-      setSelectedSubscription(null);
-      setShowEditModal(false);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleAdd = () => {
+    history.push('/subscriptions/form');
   };
 
-  const handleShowDeleteModal = (id) => {
-    setSelectedSubscriptionId(id);
-    setShowDeleteModal(true);
+  const handleEdit = (id) => {
+    history.push(`/subscriptions/form/${id}`);
   };
 
-  const handleShowEditModal = (item) => {
-    setSelectedSubscription(item);
-    setMethod('PUT');
-    setShowForm(true);
+  const handleDelete = (id) => {
+    deleteSubscription(id);
     setShowDeleteModal(false);
-  };
-
-  const handleEdit = (item) => {
-    setSelectedSubscription(item);
-    setMethod('PUT');
-    setShowEditModal(true);
-  };
-
-  const handleShowForm = () => {
-    setShowForm(true);
-  };
-
-  const handleDelete = () => {
-    deleteSubscription(selectedSubscriptionId);
-    setShowDeleteModal(false);
-    setSelectedSubscriptionId(null);
   };
 
   useEffect(() => {
     getSubscriptions();
-    getClasses();
-    getMembers();
   }, []);
 
   return (
@@ -193,7 +85,7 @@ const Subscriptions = () => {
       />
       <div className={styles.buttonContainer}>
         <h2>Subscriptions</h2>
-        <button className={styles.addSubs} onClick={handleShowForm}>
+        <button className={styles.addSubs} onClick={handleAdd}>
           Add New Subscription
         </button>
       </div>
