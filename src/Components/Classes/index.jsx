3ab5e3aacd6b0898a909968633cbe46e5fb0e67b
Daniel Ninfi <dbninfi@gmail.com>
2023-06-09 03:12:09 -0300

    MR-80: fix vercel

--- src/Components/Classes/index.jsx
+++ src/Components/Classes/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import styles from './classes.module.css';
-import Table from '../Shared/Table';
+import Table from '../Shared/Table/index';
 import Button from '../Shared/Button';
 import SharedModal from '../Shared/Modal';
 import { useHistory } from 'react-router-dom';
@@ -8,47 +8,105 @@ import { useHistory } from 'react-router-dom';
 const Classes = () => {
   const history = useHistory();
   const [classes, setClasses] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-
-  // const flagForm = () => {
-  //   history.push('/classes/form');
-  // };
+  const [showDeleteWarning, setShowDeleteWarning] = useState(false);
+  const [classId, setClassId] = useState();
+  const [modalInformation, setModalInformation] = useState({ title: '', body: '' });
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
 
   const getClasses = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/classes`);
-    const data = await response.json();
-    setClasses(data.data);
-  };
-  useEffect(() => {
-    getClasses();
-  });
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
-  const deleteItem = async (id) => {
     try {
-      await fetch(`${process.env.REACT_APP_API_URL}/api/classes/${id}`, {
-        method: 'DELETE'
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/classes`);
+      const responseData = await response.json();
+      const data = responseData.data;
+
+      const transformedData = Object.values(data).map((classItem, index) => {
+        return {
+          ...classItem,
+          _id: index.toString()
+        };
       });
-      setClasses([...classes.filter((classes) => classes._id !== id)]);
-      setModalMessage('Class deleted successfully');
-      setShowModal(true);
+
+      setClasses(transformedData);
     } catch (error) {
-      console.error('Error deleting item:', error);
-      setModalMessage('Error deleting classes');
-      setShowModal(true);
+      console.error(error);
     }
   };
 
-  const editItem = (id) => {
-    history.push(`/classes/form/${id}`);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/classes`);
+        const responseData = await response.json();
+        const data = responseData.data;
+
+        const transformedData = Object.values(data).map((classItem, index) => {
+          return {
+            ...classItem,
+            _id: index.toString()
+          };
+        });
+
+        setClasses(transformedData);
+        console.log('las clases aca', classes);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getClasses();
+    fetchData();
+  }, []);
+
+  const deleteClasses = async (id) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/classes/${id}`, {
+        method: 'DELETE'
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        setAlertMessage(data.message);
+        setShowAlert(true);
+      } else {
+        setAlertMessage(data.message);
+        setShowSuccessAlert(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleAddClass = () => {
     history.push('/classes/form');
+  };
+
+  const handleUpdateClass = (id) => {
+    history.push(`/classes/form/${id}`);
+  };
+
+  const handleDeleteClass = (id) => {
+    setModalInformation({ title: 'Warning', body: 'Are you sure?' });
+    setShowDeleteWarning(true);
+    setClassId(id);
+  };
+
+  const closeDeleteWarning = () => {
+    setShowDeleteWarning(false);
+  };
+
+  const confirmDeleteClass = async () => {
+    try {
+      await deleteClasses(classId);
+      setClasses((prevClasses) => prevClasses.filter((classItem) => classItem._id !== classId));
+      setShowDeleteWarning(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleExitAlert = () => {
+    setShowSuccessAlert(false);
   };
 
   return (
@@ -56,14 +114,43 @@ const Classes = () => {
       <>
         <h2>Classes</h2>
         <Button clickAction={handleAddClass} text="Add New" type="add" />
-        {classes && classes.length > 0 ? (
-          <Table data={classes} deleteItem={deleteItem} editItem={editItem} />
-        ) : classes === undefined ? (
-          <p>Loading...</p>
-        ) : (
-          <p>No classes found.</p>
-        )}
-        {showModal && <SharedModal message={modalMessage} onClose={closeModal} />}
+        <Table
+          data={classes || []}
+          properties={[
+            'classes._id',
+            'classes.activity.name',
+            'classes.day',
+            'classes.hour',
+            'classes.slots',
+            'classes.trainer'
+          ]}
+          columnTitles={['Activity', 'Day', 'Hour', 'Slots', 'Trainer']}
+          handleUpdateItem={handleUpdateClass}
+          handleDeleteItem={handleDeleteClass}
+          keyProperty="_id"
+        />
+        <SharedModal
+          show={showDeleteWarning}
+          closeModal={closeDeleteWarning}
+          onConfirm={confirmDeleteClass}
+          title={modalInformation.title}
+          body={modalInformation.body}
+          isDelete={true}
+        />
+        <SharedModal
+          isDelete={false}
+          show={showSuccessAlert}
+          closeModal={handleExitAlert}
+          title={'Success'}
+          body={alertMessage}
+        />
+        <SharedModal
+          isDelete={false}
+          show={showAlert}
+          closeModal={handleExitAlert}
+          title={'Something is wrong'}
+          body={alertMessage}
+        />
       </>
     </section>
   );
