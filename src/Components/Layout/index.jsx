8738d3d20e5d0d1bdb5c5f737724ebe3e3d95721
EmaBaldor <emabaldor@gmail.com>
2023-06-10 15:08:14 -0300

    MR-110: modified store, provider and added redux-devtools

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,17 +1,13 @@
 import Header from '../Header/index';
 import styles from './layout.module.css';
 import Routes from '../../Routes';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
 
 const Layout = () => {
   return (
-    <Provider store={store}>
-      <div className={styles.container}>
-        <Header />
-        <Routes />
-      </div>
-    </Provider>
+    <div className={styles.container}>
+      <Header />
+      <Routes />
+    </div>
   );
 };
 
--- src/Components/redux/store.js
+++ src/Components/redux/store.js
@@ -1,9 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
