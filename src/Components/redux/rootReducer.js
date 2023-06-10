26aa1a962d467f779b7ca9368b9c53e646ab2205
EmaBaldor <emabaldor@gmail.com>
2023-06-10 13:20:23 -0300

    MR-110: added reducers in rootReducer

--- src/Components/redux/rootReducer.js
+++ src/Components/redux/rootReducer.js
@@ -4,6 +4,11 @@ import membersReducer from './members/reducer';
 
 const rootReducer = combineReducers({
   members: membersReducer
+  // ,admins: adminsReducer,
+  // ,superAdmins: superAdminsReducer
+  // ,activities: activitiesReducer
+  // ,classes: classesReducer
+  // ,subscriptions: subscriptionsReducer
 });
 
 export default rootReducer;
