981ace96c6f6d0262ca8f4b1e2fda035c44930e6
EmaBaldor <emabaldor@gmail.com>
2023-06-10 12:31:16 -0300

    MR-120: redux implementation delete members

--- src/Components/redux/members/actions.js
+++ src/Components/redux/members/actions.js
@@ -1,4 +1,11 @@
-import { GET_MEMBERS_PENDING, GET_MEMBERS_SUCCESS, GET_MEMBERS_ERROR } from './constants';
+import {
+  GET_MEMBERS_PENDING,
+  GET_MEMBERS_SUCCESS,
+  GET_MEMBERS_ERROR,
+  DELETE_MEMBER_PENDING,
+  DELETE_MEMBER_SUCCESS,
+  DELETE_MEMBER_ERROR
+} from './constants';
 
 export const getMembersPending = () => {
   return {
@@ -16,6 +23,26 @@ export const getMembersSuccess = (data) => {
 export const getMembersError = (error) => {
   return {
     type: GET_MEMBERS_ERROR,
+    payload: error
+  };
+};
+
+export const deleteMemberPending = () => {
+  return {
+    type: DELETE_MEMBER_PENDING
+  };
+};
+
+export const deleteMemberSuccess = (id) => {
+  return {
+    type: DELETE_MEMBER_SUCCESS,
+    payload: id
+  };
+};
+
+export const deleteMemberError = (error) => {
+  return {
+    type: DELETE_MEMBER_ERROR,
     payload: error
   };
 };
--- src/Components/redux/members/thunks.js
+++ src/Components/redux/members/thunks.js
@@ -1,16 +1,41 @@
-import { getMembersPending, getMembersSuccess, getMembersError } from './actions';
+import {
+  getMembersPending,
+  getMembersSuccess,
+  getMembersError,
+  deleteMemberPending,
+  deleteMemberSuccess,
+  deleteMemberError
+} from './actions';
 
 export const getMembers = () => {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(getMembersPending());
-    return fetch(`${process.env.REACT_APP_API_URL}/members`)
-      .then((response) => response.json())
-      .then((response) => {
-        dispatch(getMembersSuccess(response.data));
-        return response.data;
-      })
-      .catch((error) => {
-        dispatch(getMembersError(error.toString()));
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/members`);
+      const data = await response.json();
+      dispatch(getMembersSuccess(data));
+      return data;
+    } catch (error) {
+      dispatch(getMembersError(error.toString()));
+    }
+  };
+};
+
+export const deleteMember = (id) => {
+  return async (dispatch) => {
+    dispatch(deleteMemberPending());
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/members/${id}`, {
+        method: 'DELETE'
       });
+
+      if (!response.ok) {
+        throw new Error('An error occurred while trying to delete the member');
+      }
+
+      dispatch(deleteMemberSuccess(id));
+    } catch (error) {
+      dispatch(deleteMemberError(error.toString()));
+    }
   };
 };
