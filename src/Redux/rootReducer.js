2c1a3f58664c2b44803ce372a4727b947662d1db
Alejandro Schmidt <alemusho@gmail.com>
2023-06-12 17:20:57 -0300

    MR-114/routes fixed

--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,15 +1,21 @@
 import { combineReducers } from 'redux';
 
 import membersReducer from './members/reducer';
+import trainersReducer from './trainers/reducer';
+import activitiesReducer from './activities/reducer';
+import superAdminReducer from './superadmins/reducer';
+import subscriptionsReducer from './subscriptions/reducer';
+import classesReducer from './classes/reducer';
 import adminsReducer from './admins/reducer';
 
 const rootReducer = combineReducers({
+  admins: adminsReducer,
+  activities: activitiesReducer,
+  classes: classesReducer,
   members: membersReducer,
-  admins: adminsReducer
-  // ,superAdmins: superAdminsReducer
-  // ,activities: activitiesReducer
-  // ,classes: classesReducer
-  // ,subscriptions: subscriptionsReducer
+  subscriptions: subscriptionsReducer,
+  superAdmin: superAdminReducer,
+  trainers: trainersReducer
 });
 
 export default rootReducer;
