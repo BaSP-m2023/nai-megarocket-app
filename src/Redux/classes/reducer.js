ffeca1a964036d06df5357941ee4766ca4900c28
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2023-06-13 09:28:53 -0300

    MR-118: added redux actions, reducer and thunks

--- src/Redux/classes/reducer.js
+++ src/Redux/classes/reducer.js
@@ -1,4 +1,4 @@
-import { GET_CLASSES_PENDING, GET_CLASSES_SUCCESS, GET_CLASSES_ERROR } from './constants';
+import * as types from './constants';
 
 const INITIAL_STATE = {
   loading: false,
@@ -8,22 +8,54 @@ const INITIAL_STATE = {
 
 const classesReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case GET_CLASSES_PENDING:
+    case types.GET_CLASSES_PENDING:
+    case types.GET_CLASS_BY_ID_PENDING:
       return {
         ...state,
-        loading: true,
-        error: null
+        loading: true
       };
-    case GET_CLASSES_SUCCESS:
+    case types.GET_CLASSES_SUCCESS:
+    case types.GET_CLASS_BY_ID_SUCCESS:
       return {
         ...state,
         loading: false,
         data: action.payload
       };
-    case GET_CLASSES_ERROR:
+    case types.GET_CLASSES_ERROR:
+    case types.GET_CLASS_BY_ID_ERROR:
       return {
         ...state,
         loading: false,
+        error: action.payload
+      };
+    case types.DELETE_CLASS_SUCCESS:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          data: state.data.data.filter((gymClass) => gymClass._id !== action.payload)
+        }
+      };
+    case types.PUT_CLASS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case types.POST_CLASS_PENDING:
+      return {
+        ...state,
+        loading: true
+      };
+    case types.POST_CLASS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case types.POST_CLASS_ERROR:
+      return {
+        ...state,
         error: action.payload
       };
     default:
