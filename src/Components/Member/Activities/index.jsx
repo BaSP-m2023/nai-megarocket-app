1791ac223cf7119eb5da2f76f3557b616e8c296e
Daniel Ninfi <dbninfi@gmail.com>
2023-06-19 12:10:22 -0300

    MR-163: retrieve data and design/layout activities screen

--- src/Components/Member/Activities/index.jsx
+++ src/Components/Member/Activities/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './activities.module.css';
+import { getActivities } from 'Redux/activities/thunks';
+import { useSelector, useDispatch } from 'react-redux';
+//import ClipLoader from 'react-spinners/ClipLoader';
 
 const Activities = () => {
-  return (
-    <div className={styles.body}>
-      <h1 className={styles.tittle}>Activities</h1>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const activities = useSelector((state) => state.activities.data.data);
+
+  useEffect(() => {
+    dispatch(getActivities());
+  }, []);
+  console.log('activities: ', activities);
+
+  if (!activities) {
+    // spinners goes here.
+    return <h2>No available activities</h2>;
+  } else {
+    return (
+      <div className={styles.body}>
+        <h1 className={styles.tittle}>Activities</h1>
+        {activities.map((activity, idx) => (
+          <div key={idx}>
+            <h2>{activity.name}</h2>
+            <p>{activity.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Activities;
