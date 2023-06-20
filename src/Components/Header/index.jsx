564f6a3f601f1982ce4eb885ba61a49ecad01c48
Marco Herazo <marcoherazo11@gmail.com>
2023-06-20 15:14:23 -0300

    MR-170: fix header component

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,29 +2,27 @@ import styles from './header.module.css';
 
 function Header() {
   return (
-    <header>
-      <div className={styles.container}>
-        <div className={styles.brand}>MEGAROCKET</div>
-        <div>
-          <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
-            />
-          </a>
-          <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
-            />
-          </a>
-          <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
-            />
-          </a>
-        </div>
+    <header className={styles.container}>
+      <div className={styles.brand}>MEGAROCKET</div>
+      <div className={styles.icons}>
+        <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
+          />
+        </a>
+        <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
+          />
+        </a>
+        <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
+          />
+        </a>
       </div>
     </header>
   );
