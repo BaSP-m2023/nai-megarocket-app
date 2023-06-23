8f08402dbab4b58a31d4ae1194d9cb9b3ebe0ed5
Daniel Ninfi <dbninfi@gmail.com>
2023-06-22 21:56:28 -0300

    MR-169: add email regex and fix login style

--- src/Validations/login.jsx
+++ src/Validations/login.jsx
@@ -1,10 +1,14 @@
 import Joi from 'joi';
 
 const loginValidation = Joi.object({
-  email: Joi.string().regex(/.+/).label('Email').messages({
-    'any.required': 'Email is required.',
-    'string.empty': 'Email is required.'
-  }),
+  email: Joi.string()
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.com$/)
+    .label('Email')
+    .messages({
+      'string.pattern.base': "Invalid email address format, must finish in '.com'",
+      'any.required': 'Email is required.',
+      'string.empty': 'Email is required.'
+    }),
   password: Joi.string().regex(/.+/).min(8).max(16).label('Password').messages({
     'any.required': 'Password is required.',
     'string.empty': 'Password is required.'
