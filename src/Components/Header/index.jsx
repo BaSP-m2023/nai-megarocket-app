c00227ae7fa1f2cd87ee11d164cef5b35e8e0ef4
Marco Herazo <marcoherazo11@gmail.com>
2023-06-22 22:39:58 -0300

    MR-170: fix header styles

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,35 +2,33 @@ import styles from './header.module.css';
 
 function Header() {
   return (
-    <header>
-      <div className={styles.container}>
-        <div className={styles.brand}>MEGAROCKET</div>
-        <div>
-          <a href={'/login'} rel="noopener noreferrer">
-            <img
-              className={styles.logIn}
-              src={`${process.env.PUBLIC_URL}/assets/images/log-in.svg`}
-            />
-          </a>
-          <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
-            />
-          </a>
-          <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
-            />
-          </a>
-          <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
-            />
-          </a>
-        </div>
+    <header className={styles.container}>
+      <div className={styles.brand}>MEGAROCKET</div>
+      <div>
+        <a href={'/login'} rel="noopener noreferrer">
+          <img
+            className={styles.logIn}
+            src={`${process.env.PUBLIC_URL}/assets/images/log-in.svg`}
+          />
+        </a>
+        <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
+          />
+        </a>
+        <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
+          />
+        </a>
+        <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
+          />
+        </a>
       </div>
     </header>
   );
