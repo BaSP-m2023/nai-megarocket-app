18b1e04165c0683f2e377666bd5a083b13952675
Daniel Ninfi <dbninfi@gmail.com>
2023-06-23 01:25:56 -0300

    MR-184: fix bug

--- src/Components/Member/Activities/index.jsx
+++ src/Components/Member/Activities/index.jsx
@@ -3,6 +3,7 @@ import styles from './activities.module.css';
 import { getActivities } from 'Redux/activities/thunks';
 import { useSelector, useDispatch } from 'react-redux';
 import Container from 'Components/Shared/Container';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const Activities = () => {
   const dispatch = useDispatch();
@@ -18,40 +19,39 @@ const Activities = () => {
     dispatch(getActivities());
   }, []);
 
-  return !activities ? (
-    <Container>
-      <div>
-        <h2 className={styles.noActivitiesTitle}>No available activities</h2>
-        <p className={styles.noActivitiesParagraph}>
-          Sorry, at this moment we have no published activities.
-        </p>
-      </div>
-    </Container>
-  ) : (
+  return (
     <Container>
       <div className={styles.container}>
-        <div className={styles.headerContainer}>
-          <div className={styles.header}>
-            <h1 className={styles.title}>Activities</h1>
-            <div className={styles.line}></div>
-            <p className={styles.paragraph}>
-              These are all the activities available. If you want to try any of them subscribe to a
-              class!
-            </p>
-          </div>
-        </div>
-        <div className={styles.cardsContainer}>
-          {activities.map((activity, idx) => (
-            <div
-              key={idx}
-              className={`${styles.card} ${styles.cardsAnimation} ${getColorClass(idx)}`}
-            >
-              <h2 className={styles.cardTitle}>{activity.name}</h2>
-              <hr />
-              <p className={styles.cardDescription}>{activity.description}</p>
+        {activities ? (
+          <>
+            <div className={styles.headerContainer}>
+              <div className={styles.header}>
+                <h1 className={styles.title}>Activities</h1>
+                <div className={styles.line}></div>
+                <p className={styles.paragraph}>
+                  These are all the activities available. If you want to try any of them subscribe
+                  to a class!
+                </p>
+              </div>
             </div>
-          ))}
-        </div>
+            <div className={styles.cardsContainer}>
+              {activities.map((activity, idx) => (
+                <div
+                  key={idx}
+                  className={`${styles.card} ${styles.cardsAnimation} ${getColorClass(idx)}`}
+                >
+                  <h2 className={styles.cardTitle}>{activity.name}</h2>
+                  <hr />
+                  <p className={styles.cardDescription}>{activity.description}</p>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className={styles.spinner}>
+            <ClipLoader />
+          </div>
+        )}
       </div>
     </Container>
   );
