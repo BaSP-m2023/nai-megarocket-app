e26736c96159c15fd53c577d19e447b3912754c0
Alejandro Schmidt <alemusho@gmail.com>
2023-06-25 13:27:57 -0300

    MR-185: onClick to clickAction name in button changed

--- src/Components/Admin/Classes/Form/index.jsx
+++ src/Components/Admin/Classes/Form/index.jsx
@@ -166,7 +166,7 @@ const Form = () => {
           <Button type={'submit'} info={'submit'} text={id ? 'Update' : 'Add'} />
           <div className={styles.confirmButton}>
             <Button type="cancel" text="Back" clickAction={handleCancel} />
-            <Button type={'cancel'} onClick={() => reset()} info={'reset'} text={'Reset'} />
+            <Button type={'cancel'} clickAction={() => reset()} info={'reset'} text={'Reset'} />
           </div>
         </div>
       </SharedForm>
