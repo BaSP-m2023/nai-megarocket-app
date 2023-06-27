5cec30a5721c1b9b8be4c45b84afb2b40afe6edb
AraEfe <ara.fnz@live.com.ar>
2023-06-27 20:31:14 -0300

    Revert "reverted previus commit"
    This reverts commit 9b5ea6d8252554ce2db3efdde53d44e231c280cb.

--- test/pageobjects/adminNavBarPage.js
+++ test/pageobjects/adminNavBarPage.js
@@ -4,17 +4,11 @@ class AdminNavBarPage {
     return browser.url(url);
   }
 
-  get adminButton() {
-    return $$('.button.sideBar_button__nJ3d+')[1]; //selector provisorio, faltan implementar IDs
-  }
-
-  get activityOptionButton() {
-    return $$('li:first-child a:nth-child(3)')[0]; //selector provisorio, faltan implementar IDs
-  }
-
-  get classOptionButton() {
-    return $$('li:first-child a:nth-child(4)')[0]; //selector provisorio, faltan implementar IDs
+  get chooseAdminButton() {
+    const buttons = $$('button.sideBar_button__nJ3d+');
+    const adminButton = buttons[1];
+    return adminButton;
   }
 }
 
-module.exports = new AdminNavBarPage();
+export default new AdminNavBarPage();
--- test/specs/adminActivity.js
+++ test/specs/adminActivity.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-undef */
-const LoginPage = require('../pageobjects/loginPage');
-const AdminNavBarPage = require('../pageobjects/adminNavBarPage');
+const LoginPage = require("../pageobjects/loginPage");
 
-describe('LoginPage', () => {
-  it('should login with correct email and password', async () => {
-    await LoginPage.open('https://nai-megarocket-app.vercel.app');
+describe("LoginPage", () => {
+  it("should login with correct email and password", async  () => {
+    await LoginPage.open("https://nai-megarocket-app.vercel.app/");
     await LoginPage.linkLogin.waitForDisplayed();
     await LoginPage.linkLogin.waitForEnabled();
     await LoginPage.singIn();
@@ -12,27 +10,6 @@ describe('LoginPage', () => {
     await LoginPage.inputEmail.waitForEnabled();
     await LoginPage.inputPassword.waitForDisplayed();
     await LoginPage.inputPassword.waitForEnabled();
-    await LoginPage.login('admin@gmail.com', 'Firebase1!');
-  });
-
-  it('should click on Admin button on the sidebar and display the options', async () => {
-    await AdminNavBarPage.open('https://nai-megarocket-app.vercel.app/admins');
-    await AdminNavBarPage.adminButton.waitForDisplayed();
-    await AdminNavBarPage.adminButton.waitForEnabled();
-    await AdminNavBarPage.adminButton.click();
-  });
-
-  it('should click on Activities option', async () => {
-    await AdminNavBarPage.open('https://nai-megarocket-app.vercel.app/admins');
-    await AdminNavBarPage.activityOptionButton.waitForDisplayed();
-    await AdminNavBarPage.activityOptionButton.waitForEnabled();
-    await AdminNavBarPage.activityOptionButton.click();
-  });
-
-  it('should click on Activities option', async () => {
-    await AdminNavBarPage.open('https://nai-megarocket-app.vercel.app/admins');
-    await AdminNavBarPage.activityOptionButton.waitForDisplayed();
-    await AdminNavBarPage.activityOptionButton.waitForEnabled();
-    await AdminNavBarPage.activityOptionButton.click();
+    await LoginPage.login("admin@gmail.com", "Firebase1!");
   });
 });
