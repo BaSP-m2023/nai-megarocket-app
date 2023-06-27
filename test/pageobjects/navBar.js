6378e5badb00d7c7b475e52d2e7ed7e86d0669eb
LucreciaVila <lucreciavilaf@gmail.com>
2023-06-27 17:30:58 -0300

    added navigation bar

---
+++ test/pageobjects/navBar.js
@@ -0,0 +1,25 @@
+class NavBar {
+  get navBarDisplayed() {
+    return $('#root > div > aside > nav');
+  }
+  get adminBtn() {
+    return $('#root > div > aside > nav > ul > button:nth-child(2)');
+  }
+  get membersAdminBtn() {
+    return $('#root > div > aside > nav > ul > li:nth-child(2) > a');
+  }
+  get profileAdminBtn() {
+    return $('#root > div > aside > nav > ul > li:nth-child(5) > a');
+  }
+
+  async adminBtnClick() {
+    await this.adminBtn.click();
+  }
+  async membersAdmBtnClick() {
+    await this.membersAdminBtn.click();
+  }
+  async profileAdminBtnClick() {
+    await this.profileAdminBtn.click();
+  }
+}
+export default NavBar();
