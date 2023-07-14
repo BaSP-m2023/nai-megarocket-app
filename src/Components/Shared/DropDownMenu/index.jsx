40189a366987662ebdfce2c72e29d0ad654c3973
Alejandro Schmidt <alemusho@gmail.com>
2023-07-13 21:55:58 -0300

    MR-143: Pictures changes for mui icons, pictures deleted from public

--- src/Components/Shared/DropDownMenu/index.jsx
+++ src/Components/Shared/DropDownMenu/index.jsx
@@ -3,7 +3,12 @@ import { logout } from 'Redux/auth/thunks';
 import SharedModal from 'Components/Shared/Modal/index';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import Avatar from '@mui/material/Avatar';
 import './dropDownMenu.moule.css';
+import Typography from '@mui/material/Typography';
+import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const DropDownMenu = ({ userData, role, profileRoute }) => {
   const [open, setOpen] = useState(false);
@@ -13,11 +18,6 @@ const DropDownMenu = ({ userData, role, profileRoute }) => {
   const dispatch = useDispatch();
   const menuRef = useRef();
   const history = useHistory();
-
-  const userIcon = `${process.env.PUBLIC_URL}/assets/images/dropDownMenu/user.png`;
-  const userPicture = `${process.env.PUBLIC_URL}/assets/images/dropDownMenu/userPicture.png`;
-  const helpIcon = `${process.env.PUBLIC_URL}/assets/images/dropDownMenu/question.png`;
-  const logoutIcon = `${process.env.PUBLIC_URL}/assets/images/dropDownMenu/log-out.png`;
 
   useEffect(() => {
     const handler = (e) => {
@@ -55,11 +55,45 @@ const DropDownMenu = ({ userData, role, profileRoute }) => {
     history.push(profileRoute);
   };
 
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+  function stringAvatar(name) {
+    return {
+      sx: {
+        width: 65,
+        height: 65,
+        bgcolor: stringToColor(name)
+      },
+      children: (
+        <Typography variant="h1" sx={{ fontSize: 23 }}>
+          {`${name.split(' ')[0][0]}${name.split(' ')[1][0]}`}
+        </Typography>
+      )
+    };
+  }
   const DropdownItem = (props) => {
     const idString = props.text.toLowerCase().replace(/\s+/g, '-');
     return (
       <li id={`${idString}-button-drop-down-menu`} className="dropdownItem" onClick={props.onClick}>
-        <img src={props.img}></img>
+        <div>{props.img}</div>
         <a> {props.text} </a>
       </li>
     );
@@ -74,7 +108,9 @@ const DropDownMenu = ({ userData, role, profileRoute }) => {
             setOpen(!open);
           }}
         >
-          <img id="user-avatar-button-drop-down-menu" src={userPicture}></img>
+          <div className="avatar">
+            <Avatar {...stringAvatar(`${userData?.firstName} ${userData?.lastName}`)} />
+          </div>
         </div>
 
         <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
@@ -85,10 +121,22 @@ const DropDownMenu = ({ userData, role, profileRoute }) => {
           <p className="user-role">{role}</p>
           <ul>
             {profileRoute && (
-              <DropdownItem img={userIcon} text={'My Profile'} onClick={sendToProfile} />
+              <DropdownItem
+                img={<PermIdentityOutlinedIcon style={{ color: 'grey' }} />}
+                text={'My Profile'}
+                onClick={sendToProfile}
+              />
             )}
-            <DropdownItem img={helpIcon} text={'Chat Support'} onClick={sendWhatsapp} />
-            <DropdownItem img={logoutIcon} text={'Logout'} onClick={showModalLogout} />
+            <DropdownItem
+              img={<ContactSupportOutlinedIcon style={{ color: 'grey' }} />}
+              text={'Chat Support'}
+              onClick={sendWhatsapp}
+            />
+            <DropdownItem
+              img={<LogoutOutlinedIcon style={{ color: 'grey' }} />}
+              text={'Logout'}
+              onClick={showModalLogout}
+            />
           </ul>
         </div>
       </div>
