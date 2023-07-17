490f4f5efc2ac4d26e50db542fb39fa7075d7760
Daniel Ninfi <dbninfi@gmail.com>
2023-07-17 20:17:19 -0300

    MR-275: footer for app

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,6 +1,7 @@
 import Header from 'Components/Shared/Header';
 import Sidebar from 'Components/Shared/Sidebar';
 import styles from './layout.module.css';
+import Footer from 'Components/Shared/Footer';
 
 const Layout = (props) => {
   return (
@@ -8,6 +9,7 @@ const Layout = (props) => {
       <Header profileRoute={props.profileRoute} />
       <Sidebar routes={props.routes} rol={props.rol} />
       <div className={styles.center}>{props.children}</div>
+      <Footer />
     </>
   );
 };
---
+++ src/Components/Shared/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = () => (
+  <footer className={styles.footerContainer}>
+    <p>&copy;Megarocket 2023. All rights reserved &nbsp;&nbsp;||&nbsp;&nbsp; Nai & Luchito team.</p>
+  </footer>
+);
+
+export default Footer;
