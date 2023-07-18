34bd2b9f530faaabea9f9c90ea11c7b1a84ab2f8
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2023-07-17 23:50:19 -0300

    MR-273: finish modal of classes

--- src/Components/Admin/Classes/Modal/index.jsx
+++ src/Components/Admin/Classes/Modal/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
-import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
+import DeleteIcon from '@mui/icons-material/Delete';
+import BadgeIcon from '@mui/icons-material/Badge';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Button, Divider, Typography } from '@mui/material';
@@ -22,47 +22,50 @@ const CalendarModal = (props) => {
     >
       <DialogTitle
         sx={{
+          backgroundColor: '#212121',
+          color: 'white',
           display: 'flex',
           alignItems: 'center',
           fontSize: '26px',
-          fontWeight: '700',
-          padding: '10px',
+          fontWeight: '600',
+          padding: '5px',
           gap: '5px',
-          justifyContent: 'center',
-          marginLeft: '10px'
+          justifyContent: 'center'
         }}
       >
-        <CalendarMonthRoundedIcon sx={{ fontSize: '35px' }} /> {props.classTitle}
+        {props.classTitle}
       </DialogTitle>
       <Divider />
-      <DialogContent sx={{ padding: '20px 40px', minWidth: '300px', textAlign: 'center' }}>
-        <Typography variant="h6" id="alert-dialog-slide-description">
-          {props.classTrainer}
+      <DialogContent
+        sx={{
+          padding: '20px 40px',
+          color: '#212121',
+          minWidth: '300px',
+          textAlign: 'center'
+        }}
+      >
+        <Typography
+          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px' }}
+          variant="h6"
+          id="alert-dialog-slide-description"
+        >
+          <BadgeIcon sx={{ fontSize: '27px', color: '#212121' }} /> {props.classTrainer}
         </Typography>
         <Typography variant="h6" id="alert-dialog-slide-description2">
           {props.classDay?.toString().replace(/([a-z])([A-Z])/g, '$1, $2')}
         </Typography>
         <Typography variant="h6" id="alert-dialog-slide-description3">
-          {props.classHour}
-        </Typography>
-
-        <Typography variant="h6" id="alert-dialog-slide-description4">
-          {props.classSlots}
+          Hour: {props.classHour} - Slots: {props.classSlots}
         </Typography>
       </DialogContent>
       <DialogActions
         sx={{ padding: '0px 30px 15px 30px', display: 'flex', justifyContent: 'center' }}
       >
-        <Button id={props.editTestId} onClick={props.handleUpdate} variant="contained">
+        <Button id={props.editTestId} onClick={props.handleUpdate} variant="outlined">
           <EditRoundedIcon />
         </Button>
-        <Button
-          id={props.editTestId}
-          onClick={props.handleDelete}
-          variant="contained"
-          color="error"
-        >
-          <DeleteOutlineRoundedIcon />
+        <Button id={props.editTestId} color="error" onClick={props.handleDelete} variant="outlined">
+          <DeleteIcon />
         </Button>
       </DialogActions>
     </Dialog>
