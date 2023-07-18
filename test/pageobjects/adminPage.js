3886731297df4ee8b399517de50fca51865ab585
AraEfe <ara.fnz@live.com.ar>
2023-07-18 19:21:29 -0300

    MR-AF:changes

--- test/pageobjects/adminPage.js
+++ test/pageobjects/adminPage.js
@@ -1,59 +1,128 @@
 /* eslint-disable no-undef */
 class AdminPage {
+  // titulo de seccion
   get sectionTitle() {
     return $('h2');
   }
-
+  // id de tabla
   get tableAdmin() {
     return $('#admin-table');
+  }
+  //id agregar general
+  get btnAdd() {
+    return $('#admin-button-submit-form');
+  }
+  // id boton activo
+  get btnActive() {
+    return $('#admin-input-checkbox');
+  }
+  // id agregar nueva actividad
+  get btnAddActivity() {
+    return $('[id="admin-button-add-activity"][type="button"]');
+  }
+  // id agregar nueva clase
+  get btnAddClass() {
+    return $('[id="admin-classes-add-button"][type="button"]');
+  }
+  // id agregar nuevo miembro
+  get btnAddMember() {
+    return $('[id="admin-add-button"][type="button"]');
+  }
+  // id agregar nuevo entrenador
+  get btnAddTrainer() {
+    return $('[id="XXXXXXXXX"][type="button"]'); //falta ID
+  }
+  // id agregar nueva subscripcion
+  get btnAddSubscription() {
+    return $('[id="admin-add-button"][type="button"]');
+  }
+  // id boton ir hacia atras
+  get btnArrowBack() {
+    return $('#admin-form-go-back');
+  }
+  // id boton cancelan dentro del modal
+  get btnCancelModal() {
+    return $('#admin-button-close-modal');
+  }
+  // id boton de confirmar en el modal
+  get btnConfirmModal() {
+    return $('#admin-button-confirm-modal');
+  }
+  // id boton de elminar en el modal
+  get btnConfirmModalMember() {
+    return $('#admin-button-confirm-delete-modal');
+  }
+
+  get btnDeleteModal() {
+    return $('#admin-modal');
   }
 
   get btnEditProfile() {
     return $('#admin-edit-button');
   }
 
-  get btnAddActivity() {
-    return $('[id="admin-button-add-activity"][type="button"]');
+  get btnHistory() {
+    return $('#subscription-button-history');
   }
 
-  get btnSubmitProfile() {
-    return $('#admin-submit-button');
+  get btnNext() {
+    return $('#pagination-button-3');
   }
 
-  get btnAdd() {
-    return $('#admin-button-submit-form');
+  get btnNext2() {
+    return $('[data-testid="NavigateNextIcon"]');
   }
 
-  get btnAddClass() {
-    return $('[id="admin-classes-add-button"][type="button"]');
+  get btnPrevious() {
+    return $('#pagination-button-0');
   }
 
-  get btnAddMember() {
-    return $('[id="admin-add-button"][type="button"]');
+  get btnPrevious2() {
+    return $('#pagination-button-1');
   }
 
-  get inputSearchTable() {
-    return $('#table-input-search');
+  get btnReportsSubscriptions() {
+    return $('#admin-button-report-subscription');
+  }
+
+  get btnReportsMembers() {
+    return $('#admin-button-report-member');
+  }
+
+  get btnReportsActivities() {
+    return $('#admin-button-report-trainer');
+  }
+
+  get containerFormClasses() {
+    return $('#menu-day');
   }
 
   get iconEdit() {
     return $('#admin-icon-edit');
   }
 
+  get iconEditActivity() {
+    return $('#admin-icon-edit-5');
+  }
+
+  get iconEditMember() {
+    return $('#admin-icon-edit-XX');
+  }
+
+  get iconEditSubscription() {
+    return $('#admin-icon-edit-4');
+  }
+
   get iconDelete() {
     return $('#admin-icon-delete');
   }
 
-  get btnPrevious() {
-    return $('#table-button-previous');
+  get iconDeleteActivity() {
+    return $('#admin-icon-delete-5');
   }
 
-  get btnNext() {
-    return $('#table-button-next');
-  }
-
-  get btnArrowBack() {
-    return $('#admin-arrow-back');
+  get iconDeleteMember() {
+    return $('#admin-icon-delete-XX');
   }
 
   get inputActivityNameModal() {
@@ -64,32 +133,8 @@ class AdminPage {
     return $('#admin-input-description');
   }
 
-  get iconEditActivity() {
-    return $('#admin-icon-edit-5');
-  }
-
-  get iconDeleteActivity() {
-    return $('#admin-icon-delete-5');
-  }
-
   get inputActivityDescriptionModalUpdate() {
     return $('#admin-input-description');
-  }
-
-  get btnActive() {
-    return $('#admin-input-checkbox');
-  }
-
-  get btnCancelModal() {
-    return $('#admin-button-close-modal');
-  }
-
-  get btnConfirmModal() {
-    return $('#admin-button-confirm-modal');
-  }
-
-  get btnDeleteModal() {
-    return $('#admin-modal');
   }
 
   get inputDay() {
@@ -105,7 +150,7 @@ class AdminPage {
   }
 
   get inputHour13() {
-    return $('#admin-input-hour-13:00');
+    return $('#admin-input-hour-13-00');
   }
 
   get inputTrainer() {
@@ -122,6 +167,10 @@ class AdminPage {
 
   get inputActivity00() {
     return $('#admin-input-activity-0');
+  }
+
+  get inputSearchTable() {
+    return $('#input-table-search');
   }
 
   get inputSlots() {
@@ -165,7 +214,11 @@ class AdminPage {
   }
 
   get inputMembership() {
-    return $('#admin-input-memebrship');
+    return $('#admin-input-membership');
+  }
+
+  get inputMembershipClassic() {
+    return $('#admin-input-membership-Classic');
   }
 
   get inputSalary() {
@@ -176,12 +229,20 @@ class AdminPage {
     return $('#admin-input-classes');
   }
 
+  get inputClasses01() {
+    return $('#admin-input-classes-64a50a98d529825366e92e9e');
+  }
+
   get inputMember() {
     return $('#admin-input-members');
   }
 
-  get btnHistory() {
-    return $('#admin-button-toggle-inactive');
+  get selectClassActivity() {
+    return $('#admin-select-activity');
+  }
+
+  get selectClassActivityAjedrez() {
+    return $('#admin-classes-select-activity-ajedrez');
   }
 
   async fillForm() {
@@ -196,13 +257,36 @@ class AdminPage {
   async fillFormAddClass() {
     await this.inputDay.click();
     await this.inputDayMonday.click();
-    await this.inputHour.click();
-    await this.inputHour13.click();
-    await this.inputTrainer.click();
-    await this.inputTrainer01.click();
+    await this.containerFormClasses.click();
+    await browser.pause(1000);
     await this.inputActivity.click();
     await this.inputActivity00.click();
+    await browser.pause(1000);
+    await this.inputHour.click();
+    await this.inputHour13.click();
     await this.inputSlots.setValue('8');
+    await this.inputTrainer.click();
+    await this.inputTrainer01.click();
+  }
+
+  async fillFormAddMember() {
+    await this.inputFirstName.setValue('Pedro');
+    await this.inputLastName.setValue('Pascal');
+    await this.inputDni.setValue('20369963');
+    await this.inputPhone.setValue('3417897897');
+    await this.inputEmail.setValue('pedropascal@gmail.com');
+    await this.inputPassword.setValue('Pass551!');
+    await this.inputCity.setValue('Rosario');
+    await this.inputDate.setValue('12-10-1980');
+    await this.inputPostalCode.setValue('2000');
+    await this.inputMembership.click();
+    await this.inputMembershipClassic.click();
+    await this.btnActive.click();
+  }
+
+  async filterClass() {
+    await this.selectClassActivity.click();
+    await this.selectClassActivityAjedrez.click();
   }
 }
 
--- test/specs/adminTest.js
+++ test/specs/adminTest.js
@@ -3,6 +3,7 @@ const LoginPage = require('../pageobjects/loginPage');
 const LandingPage = require('../pageobjects/landingPage');
 const NavBarPage = require('../pageobjects/navBarPage');
 const AdminPage = require('../pageobjects/adminPage');
+const LogoutPage = require('../pageobjects/logoutPage');
 
 describe('Admin navigation flow', () => {
   beforeAll('Open browser', () => {
@@ -21,7 +22,7 @@ describe('Admin navigation flow', () => {
     await LoginPage.inputPassword.waitForDisplayed();
     await LoginPage.login('admin@gmail.com', 'Firebase1!');
   });
-  /*
+
   it('should click on Profile button on the sidebar, display the profile form and edit the admins phone number', async () => {
     await NavBarPage.btnProfileAdmin.waitForDisplayed();
     await NavBarPage.btnProfileAdmin.click();
@@ -30,6 +31,7 @@ describe('Admin navigation flow', () => {
     await AdminPage.inputPhone.waitForDisplayed();
     await AdminPage.fillForm();
     await AdminPage.btnSubmitProfile.click();
+    await browser.pause(1000);
   });
 
   it('should click on Activities button on the sidebar', async () => {
@@ -38,6 +40,9 @@ describe('Admin navigation flow', () => {
   });
 
   it('should add a new activity', async () => {
+    await AdminPage.inputSearchTable.click();
+    await AdminPage.inputSearchTable.setValue('aaa');
+    await browser.pause(3000);
     await AdminPage.btnAddActivity.click();
     await AdminPage.fillFormAddActivity();
     await AdminPage.btnAdd.click();
@@ -58,7 +63,9 @@ describe('Admin navigation flow', () => {
   it('should delete an activity', async () => {
     await AdminPage.iconDeleteActivity.waitForDisplayed();
     await AdminPage.iconDeleteActivity.click();
+    await browser.pause(1000);
     await AdminPage.btnConfirmModal.click();
+    await browser.pause(1000);
   });
 
   it('should click on Classes button on the sidebar', async () => {
@@ -70,16 +77,110 @@ describe('Admin navigation flow', () => {
     await AdminPage.btnAddClass.click();
     await AdminPage.fillFormAddClass();
     await AdminPage.btnAdd.click();
-  });
-*/
-  it('should click on Member button on the sidebar', async () => {
-    await NavBarPage.btnClassAdmin.waitForDisplayed();
-    await NavBarPage.btnClassAdmin.click();
+    await browser.pause(1000);
   });
 
-  it('should add a new class', async () => {
+  it('should filter the class by activity and trainer', async () => {
     await AdminPage.btnAddClass.click();
     await AdminPage.fillFormAddClass();
     await AdminPage.btnAdd.click();
+    await browser.pause(1000);
+  });
+
+  it('should click on Member button on the sidebar', async () => {
+    await NavBarPage.btnMember.waitForDisplayed();
+    await NavBarPage.btnMember.click();
+  });
+
+  it('should add a new member', async () => {
+    await AdminPage.btnAddMember.click();
+    await AdminPage.fillFormAddMember();
+    await AdminPage.btnAdd.click();
+  });
+
+  it('should edit a Member', async () => {
+    await setTimeout(async () => {
+      await AdminPage.tableAdmin.waitForDisplayed();
+    }, 15000);
+    await setTimeout(async () => {
+      await AdminPage.iconEditMember.waitForDisplayed();
+    }, 15000);
+    await AdminPage.iconEditMember.click();
+    await AdminPage.inputPhone.setValue('3412588522');
+    await AdminPage.inputEmail.setValue('new@gmail.com');
+    await AdminPage.btnAdd.click();
+    await browser.pause(1000);
+  });
+
+  it('should delete a Member', async () => {
+    await AdminPage.iconDeleteMember.waitForDisplayed();
+    await AdminPage.iconDeleteMember.click();
+    await browser.pause(1000);
+    await AdminPage.btnConfirmModalMember.click();
+    await browser.pause(1000);
+  });
+
+  it('should click on Subscription button on the sidebar', async () => {
+    await NavBarPage.btnSubscriptions.waitForDisplayed();
+    await NavBarPage.btnSubscriptions.click();
+  });
+
+  it('should add a new Subscription', async () => {
+    await NavBarPage.btnSubscriptions.waitForDisplayed();
+    await NavBarPage.btnSubscriptions.click();
+  });
+
+  it('should click and switch to the final page', async () => {
+    await AdminPage.btnNext2.click();
+    await AdminPage.btnNext2.click();
+    await browser.pause(1000);
+  });
+
+  it('should edit a Subscription', async () => {
+    await setTimeout(async () => {
+      await AdminPage.tableAdmin.waitForDisplayed();
+    }, 15000);
+    await setTimeout(async () => {
+      await AdminPage.iconEditSubscription.waitForDisplayed();
+    }, 15000);
+    await AdminPage.iconEditSubscription.click();
+
+    await AdminPage.inputClasses.click();
+    await AdminPage.inputClasses01.click();
+    await AdminPage.btnAdd.click();
+    await browser.pause(1000);
+  });
+
+  it('should click on Reports button on the sidebar', async () => {
+    await NavBarPage.btnReports.waitForDisplayed();
+    await NavBarPage.btnReports.click();
+  });
+
+  it('should click on reports buttons and show all', async () => {
+    await AdminPage.btnReportsSubscriptions.waitForDisplayed();
+    await AdminPage.btnReportsSubscriptions.click();
+    await browser.pause(1000);
+    await AdminPage.btnReportsMembers.waitForDisplayed();
+    await AdminPage.btnReportsMembers.click();
+    await browser.pause(1000);
+    await AdminPage.btnReportsActivities.waitForDisplayed();
+    await AdminPage.btnReportsActivities.click();
+  });
+
+  it('should click on Home button on the sidebar', async () => {
+    await NavBarPage.btnHomeadmin.waitForDisplayed();
+    await NavBarPage.btnHomeadmin.click();
+    await browser.pause(1000);
+  });
+
+  it('should logout succesfully', async () => {
+    await LogoutPage.btnHeaderAvatar.waitForDisplayed();
+    await LogoutPage.btnHeaderAvatar.click();
+    await browser.pause(1000);
+    await LogoutPage.btnHeaderAvatar.waitForDisplayed();
+    await LogoutPage.btnLogout.click();
+    await browser.pause(1000);
+    await LogoutPage.btnHeaderAvatar.waitForDisplayed();
+    await LogoutPage.btnLogoutModalConfirm.click();
   });
 });
