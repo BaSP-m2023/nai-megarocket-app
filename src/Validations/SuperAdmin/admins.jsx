d4558e0aaf99099453ccb7d46083aab69919bf62
Gonzarey96 <gonzareybet@gmail.com>
2023-07-19 00:45:17 -0300

    MR-281: last fix

--- src/Validations/SuperAdmin/admins.jsx
+++ src/Validations/SuperAdmin/admins.jsx
@@ -67,7 +67,7 @@ const adminsValidation = Joi.object({
     .label('Password')
     .messages({
       'string.pattern.base':
-        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol.',
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol',
       'string.min': 'Password must have at least 8 characters',
       'string.max': 'Password must have less than 16 characters',
       'any.required': 'Password is required',
